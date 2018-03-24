ab70207e9e32e294a26fef2bb655f21d4d1d6f3a
Yinglu Wang <yw2510@nyu.edu>
2018-03-24 10:36:49 -0400

    Update index.php

--- ResourceCrowd/index.php
+++ ResourceCrowd/index.php
@@ -1,20 +1,27 @@
 <html>
 <head>
   <title>Thanks for joining us!</title>
+<!--CSS-->
+  <link rel="stylesheet" type="text/css" href="resourcecrowd.css">
+<!--Font-->
+  <link href='http://fonts.googleapis.com/css?family=Kanit|Rammetto+One|Lobster' rel='stylesheet'>
+<!--Bootstrap4-->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 
-<body>
+<body class="thankyouPage">
+  <div class="thankyou text-center">Thanks   for   joining   us!</div>
+  <div class='back text-center'>Click <a href="home.html">here</a> to go back to the home page</div>
+
+  <footer class="col-12 text-center">Copyright &copy; Resource Crowd</footer>
 <?php
   error_reporting(0);
 
   $fname=$_POST['fname'];
   $lname=$_POST['lname'];
   $univ=$_POST['univ'];
-  $level=$_POST['level'];
-  $subject=$_POST['subject'];
-
-  if(is_array($level))
-    $level=implode(',',$level);
+  $email=$_POST['email'];
+  $subjects=$_POST['subject'];
 
 //Write data into Database
 //user: root pw: yw123890
@@ -32,21 +39,40 @@
   $lastname  = htmlentities($lname);
   $university = htmlentities($univ);
 
-  $firstname = mysqli_real_escape_string($connect,$firstname);  #escape all ' " \ newline
-  $lastname = mysqli_real_escape_string($connect,$lastname);   #with another \, making them
-  $university = mysqli_real_escape_string($connect,$university);
-
-  $update = "INSERT INTO stud_info (StudentID, FirstName, LastName, University, Resources_Level, Subject)
-            Values(0,'$firstname','$lastname','$university','$level','$subject')";
+  $update = "INSERT INTO stud_info (sign_up_id, FirstName, LastName, University, Email)
+            Values(0,'$firstname','$lastname','$university', '$email')";
 
   $result = mysqli_query($connect,$update);
 
   if(!$result)
     die(mysqli_error($connect));
 
-  mysqli_close($connect);
+//connect info table and subject table
+  $query = "SELECT LAST_INSERT_ID()";                           #get the last insert id to get the pts sign_up_id
 
-  print "Thanks for joining us!"
+  $cursor = mysqli_query($connect,$query);                      #execute the query
+
+  if (!$cursor)
+      die(mysqli_error($connect));
+
+  $row = mysqli_fetch_row($cursor);
+
+  $parent_order_id = $row[0];
+
+  mysqli_free_result($cursor);
+
+  foreach($subjects as $subject)
+  {
+    $insert = "INSERT INTO stud_info_subject (id_sub, Subject, Resources_Level, sign_up_id) VALUES(0,'$subject','$level',$parent_order_id)";
+    $result = mysqli_query($connect,$insert);
+
+    $levels=$_POST['level'];
+
+    if(is_array($levels))
+      $level=implode(',',$levels);
+  }
+
+  mysqli_close($connect);
 
 ?>
 
